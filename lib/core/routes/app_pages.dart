28c9c83453343208dedeef97519fd2274dcaf028
Kusal47 <kusalaryal47@gmail.com>
2025-09-13 15:31:41 +0545

    search page added

--- lib/core/routes/app_pages.dart
+++ lib/core/routes/app_pages.dart
@@ -7,6 +7,7 @@ import 'package:scan_qr/features/home/presentation/home_page.dart';
 import '../../features/onboarding/presentation/onboarding_page.dart';
 import '../../features/scan/qr_code/presentation/qr_code_generator_screen.dart';
 import '../../features/scan/qr_code/presentation/qr_scan_page.dart';
+import '../../features/search/presentation/search_history_page.dart';
 import '../../features/splash/presentation/splash_page.dart';
 
 part 'app_routes.dart';
@@ -24,6 +25,7 @@ class AppPages {
     GetPage(name: _Paths.generateQr, page: QrGeneratorPage.new),
     GetPage(name: _Paths.barcodescanScreen, page: BarcodeScanScreen.new),
     GetPage(name: _Paths.generateBarcode, page: BarcodeGenerateScreen.new),
+    GetPage(name: _Paths.searchHistoryScreen, page: SearchHistoryPage.new),
     
   ];
 }
