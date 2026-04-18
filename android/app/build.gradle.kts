912d5c3b29021c8178d6d20f504c175107dda041
Kusal47 <kusalaryal47@gmail.com>
2026-04-18 22:49:45 +0545

    feat: migrate Android build configuration to Kotlin DSL with release signing support

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,16 +16,16 @@ val keystoreProperties = Properties().apply {
 }
 android {
     namespace = "com.codova_app.ak"
-    compileSdk = 35
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
@@ -34,7 +34,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
-        targetSdk = 35
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
